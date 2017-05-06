d46974529e13a47e5224083d9d165c11419528a8
Manolo <manolototopoyo@hotmail.com>
2017-05-06 13:17:53 -0700

    init

---
+++ scales.py
@@ -0,0 +1,7 @@
+scales = {
+	'C':['C','Dm','Em','F','G','Am','B0'],
+	'Db':['Db','Ebm','Fm','Gb','Ab','Bbm','C0'],
+	'D':['D','Em','F#m','G','A','Bm','C#0'],
+	'Eb':['Eb','Fm','Gm','Ab','Bb','Cm','D0']
+	'E':['E','F#m','G#m','A','B','C#m','D#0']
+}
