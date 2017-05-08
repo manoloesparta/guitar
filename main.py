1f166424672c3f554f6edf53271f242d3a995605
Manolo <manolototopoyo@hotmail.com>
2017-05-07 22:25:26 -0700

    update capitalize chords

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
 	#You choose a chord
 	chord = input('\nChoose a chord: ')
 	#Capitalize chord
-	chord = chord.capitalize()
+	chord = chord[0].upper() + chord[1:].lower()
 	#This chord exist?
 	try:
 		scales[chord]
@@ -16,16 +16,16 @@ def main():
 	except KeyError:
 		print('That chord isnt included')
 		quit()
+	#Assign the probably chosen chords
+	prob_chords = scales[chord]
 	#How much chords are we talking about?
-	count = input('How many chords: ')
+	count = input('How many chords?')
 	count = int(count)
 	#Creating the new chord progression
-	progression = []
-	progression += random.sample(scales[chord],count)
-	#Print progression
-	for i in progression:
-		print(i, end=' ')
-	print('\n')
+	progression = ''
+	for i in range(int(count)):
+		progression += random.choice(prob_chords) + ' '
+	print(progression)
 
 if __name__ == '__main__':
 	main()
